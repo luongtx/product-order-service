a70b21d1f383a6826adbde747b318b49204dcafa
Luong01 <txluong01@IfiSolution.local>
2022-04-04 17:53:34 +0700

    enable eureka client

--- src/main/java/com/demo/product_order_service/controller/ProductOrderApiController.java
+++ src/main/java/com/demo/product_order_service/controller/ProductOrderApiController.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -31,7 +32,7 @@ public class ProductOrderApiController implements ProductOrderApi {
 
     private final HttpServletRequest request;
 
-    @org.springframework.beans.factory.annotation.Autowired
+    @Autowired
     public ProductOrderApiController(ObjectMapper objectMapper, HttpServletRequest request) {
         this.objectMapper = objectMapper;
         this.request = request;
